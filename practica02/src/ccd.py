d8090bf56bad0ff830b5c342b730c4c06ff5dd40
miguel141097 <j2mc1410@gmail.com>
2020-12-24 11:10:44 +0000

    Movimiento del brazo

--- practica02/src/ccd.py
+++ practica02/src/ccd.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Robótica Computacional - Curso 2014/2015
+# Robótica Computacional - Curso 2020/2021
 # Grado en Ingeniería Informática (Cuarto)
 # Práctica: Resolución de la cinemática inversa mediante CCD
 #           (Cyclic Coordinate Descent).
 
 import sys
-from math import *
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import colorsys as cs
@@ -33,68 +33,92 @@ def muestra_robot(O,obj):
     plt.plot(T[i][0], T[i][1], '-o', color=cs.hsv_to_rgb(i/float(len(T)),1,1))
   plt.plot(obj[0], obj[1], '*')
   plt.show()
-  raw_input()
+  input()
   plt.clf()
 
-def matriz_T(d,th,a,al):
-  # Calcula la matriz T (ángulos de entrada en grados)
-  
-  return [[cos(th), -sin(th)*cos(al),  sin(th)*sin(al), a*cos(th)]
-         ,[sin(th),  cos(th)*cos(al), -sin(al)*cos(th), a*sin(th)]
-         ,[      0,          sin(al),          cos(al),         d]
-         ,[      0,                0,                0,         1]
-         ]
 
-def cin_dir(th,a):
-  #Sea 'th' el vector de thetas
-  #Sea 'a'  el vector de longitudes
+# Calcula la matriz T
+# Los ángulos de entrada deben estar en grados
+def matriz_T(d, th, a, al):
+  return  [ 
+            [math.cos(th), - math.sin(th) * math.cos(al),   math.sin(th) * math.sin(al), a * math.cos(th)],
+            [math.sin(th),   math.cos(th) * math.cos(al), - math.sin(al) * math.cos(th), a * math.sin(th)],
+            [           0,                  math.sin(al),                  math.cos(al),                d],
+            [           0,                             0,                             0,                1]
+          ]
+
+
+#Sea 'th' el vector de thetas
+#Sea 'a'  el vector de longitudes
+def cin_dir(th, a):
   T = np.identity(4)
-  o = [[0,0]]
+  o = [[0, 0]]
   for i in range(len(th)):
-    T = np.dot(T,matriz_T(0,th[i],a[i],0))
-    tmp=np.dot(T,[0,0,0,1])
-    o.append([tmp[0],tmp[1]])
+    T = np.dot(T, matriz_T(0, th[i], a[i], 0))
+    tmp = np.dot(T, [0,0,0,1])
+    o.append([tmp[0], tmp[1]])
   return o
+
+
+def obtener_angulo(punto_a, punto_b, punto_c):
+  vector_ac = np.subtract(punto_a, punto_c)
+  vector_bc = np.subtract(punto_b, punto_c)
+
+  alfa_1 = math.atan2(vector_ac[1], vector_ac[0])
+  alfa_2 = math.atan2(vector_bc[1], vector_bc[0])
+
+  beta = alfa_1 - alfa_2
+  return beta
+
 
 # ******************************************************************************
 # Cálculo de la cinemática inversa de forma iterativa por el método CCD
 
-# valores articulares arbitrarios para la cinemática directa inicial
-th=[0.,0.,0.]
-a =[5.,5.,5.]
-L = sum(a) # variable para representación gráfica
-EPSILON = .01
+# Valores arbitrarios de las articulares para la cinemática directa inicial
+# Los gardos están en radianes
+th = [0.0, 0.0, 0.0]
+a  = [5.0, 5.0, 5.0]
+
+# Variable para representación gráfica
+L  = sum(a)
+EPSILON = 0.01
 
 plt.ion() # modo interactivo
 
-# introducción del punto para la cinemática inversa
+# Introducción del punto para la cinemática inversa
 if len(sys.argv) != 3:
   sys.exit("python " + sys.argv[0] + " x y")
-objetivo=[float(i) for i in sys.argv[1:]]
+objetivo = [float(i) for i in sys.argv[1:]]
 
-O=range(len(th)+1) # Reservamos estructura en memoria
-O[0]=cin_dir(th,a) # Calculamos la posicion inicial
+# Reserva una estructura en memoria, para las posiciones de las articulaciones
+O = list(range(len(th) + 1)) 
+
+# Calcula la posicion inicial
+O[0] = cin_dir(th, a) 
 print("- Posicion inicial:")
 muestra_origenes(O[0])
 
 dist = float("inf")
-prev = 0.
+prev = 0.0
 iteracion = 1
-while (dist > EPSILON and abs(prev-dist) > EPSILON/100.):
-  prev = dist
-  # Para cada combinación de articulaciones:
+
+while (dist > EPSILON and abs(prev - dist) > EPSILON/100.0):
+  prev = dist  
+
+  final_index = len(th)
+
+  # Para cada combinación de articulaciones
   for i in range(len(th)):
-    # cálculo de la cinemática inversa:
+    indice_articulacion_actual = len(th) - 1 - i
+
+    punto_final = O[i][-1]
+    efector = O[i][indice_articulacion_actual]
+
+    variacion_angulo = obtener_angulo(objetivo, punto_final, efector)
+    th[indice_articulacion_actual] += variacion_angulo
     
-    
-    
-    
-    
-    
-    
-    
-    
-    O[i+1] = cin_dir(th,a)
+    O[i+1] = cin_dir(th, a)
+
 
   dist = np.linalg.norm(np.subtract(objetivo,O[-1][-1]))
   print("\n- Iteracion " + str(iteracion) + ':')
